71ef051a969b5a418653b8ceba740c3689485063
zlmi <15910344021@163.com>
2019-02-28 18:55:09 +0800

    日常使用

---
+++ BootStrapDemo/js/bs-H5.js
@@ -0,0 +1,12 @@
+$(function(){
+   // var flag = false;
+    $("#about-me img").click(function(){
+        //alert("x");
+      //  if(flag == false){
+            $(this).css("width","auto");
+            $(this).css("height","auto");
+            //flag = true;
+       // }
+        
+    })
+})
