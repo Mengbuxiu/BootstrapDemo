bcbb3cbcca423a160242940a3e1f4bfc0bde55de
zlmi <15910344021@163.com>
2019-03-21 17:34:21 +0800

    json-test

--- BootStrapDemo/js/bs-H5.js
+++ BootStrapDemo/js/bs-H5.js
@@ -1,50 +1,40 @@
 $(function () {
   // var flag = false;
-  $("#about-me img").click(function () {
-    //alert("x");
-    //  if(flag == false){
-    $(this).css("width", "auto");
-    $(this).css("height", "auto");
-    //flag = true;
-    // }
+  $("#about-me").click(function () {
+    let arr = ["A","b"];
+    console.log(arr.indexOf("c"))
+    console.log(arr.indexOf("b"))
+    let json = {
+      "a": 1,
+      "b": 2
+    };
+    let obj = { c: 3 };
+    obj['aaa']='aaa'; 
+    console.log(obj)
+    console.log(JSON.stringify(obj));
+
+    json['f'] = JSON.stringify(obj);
+    console.log(json);
+
+    
+    // let obj = { c: 3 ,c2: 3 ,c1: 3 ,};
+    // let json = {
+    //     "a": 1,
+    //     "b": 2,
+    //     ...obj
+    //   };
+    //   console.log(json)
   })
 
-  console.log($("#filter-test p").filter(".border-line"));
-  
+
+  //var currentDateLong = new Date(currentdate.replace(new RegExp("-", "gm"), "/")).getTime()     //当前时间转换成long型 
+
+  //console.log($("#filter-test p").filter(".border-line"));
 
 
-  $("button[name='test']").click(() => {
-    $('#tree').treeview({ data: getTree() });
-  })
-  })
 
-  function getTree() {
-    // Some logic to retrieve, or generate tree structure
-    return [
-            {
-              text: "Parent 1",
-              nodes: [
-                {
-                  text: `<div class="row">
-                          <a>111111</a>
-                          <a>222222</a>
-                          <a>333333</a>
-                         </div>`
-                  // nodes: [
-                  //   {
-                  //     text: "Grandchild 1"
-                  //   },
-                  //   {
-                  //     text: "Grandchild 2"
-                  //   }
-                  //]
-                },
-                {
-                  text: "Child 2"
-                }
-              ]
-            }
-          ];
-  }
 
- 
+})
+
+
+
