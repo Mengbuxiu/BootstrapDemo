b99c4a0ed1f5116e31a846cfc77b7ed9cea455e4
zlmi <15910344021@163.com>
2019-08-09 16:41:16 +0800

    正则匹配

--- ts_jike/src/index.ts
+++ ts_jike/src/index.ts
@@ -1,13 +1,26 @@
 import { Message} from "./enum";
 
-let hello:string = "hello,world";
-document.querySelectorAll(".app")[0].innerHTML = hello;
-enum Role {
-    Repoter1 = 1,
-    Developer1,
-    Maintainer1,
-    Owner1,
-    Guest1
-}
-console.log(Role.Repoter1);
-console.log(Message.Success);
+
+//以字母开头，并包含数字，长度在8-30
+
+let pwd:string = "S_1ss00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000S_1ss00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+let reg = pwd.match("^[a-zA-Z][0-9a-zA-Z_]{8,30}")
+console.log(reg);
+
+
+//正则匹配 以Sys开头的字符串
+//let hello:string = "SystemAdmin";
+// let reg = hello.match("\\Sys\\w*")
+// if(reg != null){
+//     console.log(reg)
+// }
+// document.querySelectorAll(".app")[0].innerHTML = hello;
+// enum Role {
+//     Repoter1 = 1,
+//     Developer1,
+//     Maintainer1,
+//     Owner1,
+//     Guest1
+// }
+// console.log(Role.Repoter1);
+// console.log(Message.Success);
