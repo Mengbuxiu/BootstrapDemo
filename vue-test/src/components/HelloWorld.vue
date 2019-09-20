51d74a43b1f026c28880f2974026e2cc9d9ae3c2
zlmi <15910344021@163.com>
2019-09-20 17:54:01 +0800

    vue学习

---
+++ vue-test/src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    <h3>{{ title }}</h3>
+    <label for="val"><i>TODO: </i>
+      <input id="val" v-model="newItem" @keyup.exact.enter="addVal"/>
+    </label>
+    <ul class="ul">
+      <li v-for="(item, index) in items" :key="index" v-bind:class="{finished: item.isFinished}">
+        {{index}} -> {{item.label}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      title: 'Welcome to Your Vue.js App',
+      newItem: '',
+      items: [
+        {
+          label: 'coding',
+          isFinished: false
+        },
+        {
+          label: 'working',
+          isFinished: true
+        }
+      ]
+    }
+  },
+  methods: {
+    addVal: function (val) {
+      this.items.push({label: this.newItem, isFinished: false})
+      this.newItem = ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  ul {
+    list-style: none;
+  }
+</style>
